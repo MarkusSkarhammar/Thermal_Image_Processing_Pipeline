5a5c0c704ed947454eaa4d0c5107bdde086f2e65
markussk <linkis2@hotmail.com>
2019-05-17 12:26:21 +0200

    Optimization of the image data conversion and general code cleanup.

--- app/src/main/java/com/example/thermal_image_processing_pipeline/DisplayHandler.java
+++ app/src/main/java/com/example/thermal_image_processing_pipeline/DisplayHandler.java
@@ -13,83 +13,13 @@ import java.util.ArrayList;
 import static android.graphics.Bitmap.Config.ARGB_8888;
 
 public class DisplayHandler {
-    public static int GRAY = 0, RED = -1, BLUE = -2, GREEN = -3;
     private static int[] pixels;
-    private static int[][] data;
-
-    public static Bitmap generateBitmapFromPGM(PGMImage image, int color){
-        return generateBitmap(image, color);
-    }
 
     public static Bitmap generateBitmapFromPGM(PGMImage image){
 
-        return generateBitmap(image, GRAY);
-    }
-
-    private static Bitmap generateBitmap(PGMImage image, int color){
-        long timeStampStart, timeStampEnd;
-        timeStampStart = System.currentTimeMillis();
-
         Bitmap bitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(),  ARGB_8888);
-        pixels = image.getDataList();
-        //pixels = new int[image.getWidth()*image.getHeight()];
-        //data = image.getData();
-
-        /*ArrayList<Thread> threads = new ArrayList<>();
-        int amount = 2;
-        for(int k=0; k<2; k++)
-            for(int i=0; i<amount/2; i++){
-                threads.add(getPixels(image, color, i * image.getWidth()/(amount/2), k * image.getHeight()/2, (1 + i)*image.getWidth()/(amount/2), (1 + k) * image.getHeight()/2));
-            }
-        for(Thread t : threads){
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        /*
-        Thread t1 = getPixels(image, color, 0, 0, image.getWidth()/2, image.getHeight()/2);
-        Thread t2 = getPixels(image, color, image.getWidth()/2, 0, image.getWidth(), image.getHeight()/2);
-        Thread t3 = getPixels(image, color, 0, image.getHeight()/2, image.getWidth()/2, image.getHeight());
-        Thread t4 = getPixels(image, color, image.getWidth()/2, image.getHeight()/2, image.getWidth(), image.getHeight());
-        try {
-            t1.join();
-            t2.join();
-            t3.join();
-            t4.join();
-        } catch (InterruptedException e) {
-            Log.d("Bitmap generator: ","Interrupted exception: " + e);
-        }
-        for(int x = 0; x < image.getWidth(); x++){
-            for(int y = 0; y < image.getHeight(); y++){
-                //pixels[(y*image.getWidth()) + x] = getColor(image.getDataAt(x, y), color, image, true);
-                pixels[(y*image.getWidth()) + x] = data[y][x];
-            }
-        }*/
-
-        bitmap.setPixels(pixels, 0, bitmap.getWidth(), 0, 0, image.getWidth(), image.getHeight());
-
-        timeStampEnd = System.currentTimeMillis();
-        //Log.d("TCP Client:", " Time to get image: " + (timeStampEnd - timeStampStart) + " ms.");
-
+        bitmap.setPixels(image.getDataList(), 0, bitmap.getWidth(), 0, 0, image.getWidth(), image.getHeight());
         return bitmap;
-    }
-
-    private static Thread getPixels(final PGMImage image, final int color, final int xFrom, final int yFrom, final int xTo, final int yTo){
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                for(int x = xFrom; x < xTo; x++){
-                    for(int y = yFrom; y < yTo; y++){
-                        pixels[(y*image.getWidth()) + x] = getColor(image.getDataAt(x, y), color, image, true);
-                    }
-                }
-            }
-        };
-        Thread thread = new Thread(runnable);
-        thread.start();
-        return thread;
     }
 
     public static void DrawCanvas(Bitmap b, ImageView img){
@@ -108,53 +38,6 @@ public class DisplayHandler {
             );
 
             img.setImageBitmap(b);
-        }
-    }
-
-
-
-    private static int getColor(int colorValue, int color, PGMImage image){
-        Color c = new Color();
-        if(colorValue == 255){
-            if(color == RED)
-                return Color.RED;
-            else if(color == BLUE)
-                return Color.BLUE;
-            else if(color == GREEN)
-                return Color.GREEN;
-            else
-                return Color.WHITE;
-        }else if(colorValue == 0){
-            return Color.BLACK;
-        }else{
-            if(color == RED)
-                return c.rgb(colorValue, 0, 0);
-            else if(color == BLUE)
-                return c.rgb(0, 0, colorValue);
-            else if(color == GREEN)
-                return c.rgb(0, colorValue, 0);
-            else
-                return c.rgb(colorValue, colorValue, colorValue);
-        }
-    }
-
-    private static int getColor(int pixelDensity, int color, PGMImage image, boolean andra){
-        pixelDensity = Math.abs(pixelDensity);
-        Color c = new Color();
-        int colorValue = (int)(((double)pixelDensity / 4095.0) * 255);
-        if(colorValue == 0){
-            return Color.BLACK;
-        }else if(colorValue == 255){
-            return Color.WHITE;
-        }else{
-            if(color == RED)
-                return c.rgb(colorValue, 0, 0);
-            else if(color == BLUE)
-                return c.rgb(0, 0, colorValue);
-            else if(color == GREEN)
-                return c.rgb(0, colorValue, 0);
-            else
-                return c.rgb(colorValue, colorValue, colorValue);
         }
     }
 
--- app/src/main/java/com/example/thermal_image_processing_pipeline/OpenCVHandler.java
+++ app/src/main/java/com/example/thermal_image_processing_pipeline/OpenCVHandler.java
@@ -14,9 +14,6 @@ import static org.opencv.core.CvType.CV_8UC1;
 public class OpenCVHandler {
 
     public static void equalizeHist(PGMImage img) {
-        long timeStampStart, timeStampEnd;
-
-        timeStampStart = System.currentTimeMillis();
 
         Bitmap b = DisplayHandler.generateBitmapFromPGM(img);
 
@@ -24,31 +21,15 @@ public class OpenCVHandler {
         Utils.bitmapToMat(b, src);
         Imgproc.cvtColor(src, src, Imgproc.COLOR_RGB2GRAY);
 
-        timeStampEnd = System.currentTimeMillis();
-        //Log.d("Pipeline:", " Time convert data to bitmap: " + (timeStampEnd - timeStampStart) + " ms.");
-
         //Photo.fastNlMeansDenoising(src, src);
 
-        timeStampEnd = System.currentTimeMillis();
-        //Log.d("Pipeline:", " Time to denoising: " + (timeStampEnd - timeStampStart) + " ms.");
-
-        timeStampStart = System.currentTimeMillis();
-
         Imgproc.createCLAHE(3).apply(src, src);
-
-        timeStampEnd = System.currentTimeMillis();
-        //Log.d("Pipeline:", " Time to CLAHE: " + (timeStampEnd - timeStampStart) + " ms.");
-
-        timeStampStart = System.currentTimeMillis();
 
         Imgproc.cvtColor(src, src, Imgproc.COLOR_GRAY2RGB, 4);
 
         //src.convertTo(src,-1,1,-90);
 
         Utils.matToBitmap(src, b);
-
-        timeStampEnd = System.currentTimeMillis();
-        //Log.d("Pipeline:", " Convert to bitmap: " + (timeStampEnd - timeStampStart) + " ms.");
 
         img.setProcessedBitmap(b);
     }
