ef4c94fdcc44068f875b5cbb56adac164bc9aab6
drazenm <dat15dmi@student.lu.se>
2019-06-10 14:04:06 +0200

    Finished StreamPlayer.

---
+++ app/src/main/java/com/example/thermal_image_processing_pipeline/StreamPlayer.java
@@ -0,0 +1,75 @@
+package com.example.thermal_image_processing_pipeline;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+
+import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StreamPlayer {
+
+    /*
+
+        Frame stream player.
+
+        In order to use, see the following example code:
+
+            StreamPlayer sp = new StreamPlayer(MainActivity.this, "test");
+            imageTemp = sp.play();
+
+     */
+
+    private Activity activity;
+    private String filename;
+    private ArrayList<Integer> playList = new ArrayList<Integer>();
+    private int counter = 0;
+    private int last = 0;
+
+    public StreamPlayer(Activity a, String filename) {
+
+        this.activity = a;
+        this.filename = filename;
+
+        // Check if we have read permission
+        int permission = ActivityCompat.checkSelfPermission(a, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            //Find the directory for the SD Card using the API
+            //*Don't* hardcode "/sdcard"
+            File sdcard = Environment.getExternalStorageDirectory();
+
+            //Get the text file
+            File file = new File(sdcard, "/Download/" + filename + ".play");
+
+            if (file.exists()) {
+                try {
+
+                    Scanner scan = new Scanner(file);
+
+                    while (scan.hasNextLine()) {
+                        playList.add(Integer.parseInt(scan.nextLine()));
+                    }
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                last = playList.get(playList.size() - 1);
+            }
+        }
+    }
+
+    public void play() {
+
+        for (int i = 0; i < last; ++i) {
+            String f = filename + "~" + i;
+            MainActivity.imageStreamOffline.add(FileManagement.readFile(activity, f));
+        }
+    }
+}
